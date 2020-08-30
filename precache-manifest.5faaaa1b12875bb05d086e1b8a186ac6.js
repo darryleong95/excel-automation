85ff73c9246a63c7ca872ca25c982abbecf538a5
Darryl Leong <darryleong95@gmail.com>
2020-08-30 13:57:51 +0800

    Updates

---
+++ precache-manifest.5faaaa1b12875bb05d086e1b8a186ac6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f1c7f62fb37a75a07ee0f24c8a9664",
+    "url": "/excel-automation/index.html"
+  },
+  {
+    "revision": "32d52fcd7ace1d825284",
+    "url": "/excel-automation/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "167c795af4f6537072ce",
+    "url": "/excel-automation/static/js/2.9c81f966.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/excel-automation/static/js/2.9c81f966.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32d52fcd7ace1d825284",
+    "url": "/excel-automation/static/js/main.b0dd2bc3.chunk.js"
+  },
+  {
+    "revision": "b7b839d9712b32b75946",
+    "url": "/excel-automation/static/js/runtime-main.bbbfcfe6.js"
+  }
+]);
