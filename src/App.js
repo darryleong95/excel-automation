6daaf173332511b408f6bb61ad1c20a87e70f5f1
Darryl Leong <darryleong95@gmail.com>
2020-08-31 20:13:53 +0800

    Added dynamic field quantity field generation

--- src/App.js
+++ src/App.js
@@ -4,115 +4,125 @@ import * as FileSaver from 'file-saver';
 
 const App = () => {
 
-  const [prices, setPrices] = useState([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-  const [dlFile, setDlFile] = useState()
-
   const [fileA, setFileA] = useState({})
   const [fileB, setFileB] = useState({})
 
-  const [headers, setHeaders] = useState([])
+  const [q, setQs] = useState({})
+  const [titleColumn, setTitleColumn] = useState("")
 
   const excelReader = (data, isFirstFile) => {
     let renderedData = XLSX.read(data, { type: 'binary' });
     const dataParse = XLSX.utils.sheet_to_json(renderedData.Sheets[renderedData.SheetNames[0]], { header: 1 });
 
-    setHeaders(dataParse[0])
+    setTitleColumn(dataParse[0][0])
+
+    for (let i = 1; i < dataParse[0].length; i++) {
+      let key = parseInt(dataParse[0][i])
+      if (!(key in q))
+        q[key] = 1
+    }
+
+    setQs(q)
 
     let fileDict = {}
 
     for (let i = 1; i < dataParse.length; i++) {
       let partName = dataParse[i][0]
-      fileDict[partName] = dataParse[i].slice(1)
+
+      let priceQuantity = {}
+      for (let j = 1; j < dataParse[i].length; j++) {
+        let quantity = parseInt(dataParse[0][j])
+        let value = dataParse[i][j]
+        priceQuantity[quantity] = value
+      }
+
+      fileDict[partName] = priceQuantity
     }
 
     delete fileDict[undefined]
     isFirstFile ? setFileA(fileDict) : setFileB(fileDict)
   }
 
-  const onFileChange = (e) => {
+  const onFileChange = (e, isFirstFile) => {
     e.preventDefault();
     var files = e.target.files, f = files[0];
     var reader = new FileReader();
     reader.onload = function (e) {
-      excelReader(e.target.result, true)
-    };
-    reader.readAsBinaryString(f)
-  }
-
-  const onFileChange2 = (e) => {
-    e.preventDefault();
-    var files = e.target.files, f = files[0];
-    var reader = new FileReader();
-    reader.onload = function (e) {
-      excelReader(e.target.result, false)
+      excelReader(e.target.result, isFirstFile)
     };
     reader.readAsBinaryString(f)
   }
 
   const downloadFile = () => {
     // compare 2 files and take lowest value
-
-    if (fileB != null) {
-      let fileBKeys = Object.keys(fileB)
-      for (let i = 0; i < fileBKeys.length; i++) {
-        let Bkey = fileBKeys[i]
-        if (fileA[Bkey] === undefined) {
-          fileA[Bkey] = fileB[Bkey]
-        }
-      }
-
+    if (Object.keys(fileB).length !== 0) {
       let fileAKeys = Object.keys(fileA)
-      for (let i = 0; i < fileAKeys.length; i++) {
-        let Akey = fileAKeys[i]
-        if (fileB[Akey] !== undefined) {
-          /* check for lowest value here */
-          let priceListA = JSON.parse(JSON.stringify(fileA[Akey]))
-          let priceListB = JSON.parse(JSON.stringify(fileB[Akey]))
-          let ll = priceListA.length >= priceListB.length ? priceListA : priceListB
-          let sl = priceListA.length < priceListB.length ? priceListA : priceListB
-          for (let j = 0; j < ll.length; j++) {
-            let pll = ll[j]
-            let pss = sl[j]
-            ll[j] = pll < pss ? pll : pss // select lowest price of 2
+      let fileBKeys = Object.keys(fileB)
+      let combined = fileAKeys.concat(fileBKeys)
+
+      let uniqueItems = [...new Set(combined)]
+
+      let quantities = Object.keys(q)
+
+      for (let i = 0; i < uniqueItems.length; i++) {
+        let individualItemPrice = {}
+        /* Item does not exist in File A but exits in File B */
+        if (fileA[uniqueItems[i]] === undefined) {
+          for (let j = 0; j < quantities.length; j++) {
+            individualItemPrice[quantities[j]] = fileB[uniqueItems[i]][quantities[j]]
           }
-          /*Re-assign price list to fileA */
-          fileA[Akey] = ll
         }
+        /* Item does not exist in File B but exits in File A */
+        else if (fileB[uniqueItems[i]] === undefined) {
+          for (let j = 0; j < quantities.length; j++) {
+            individualItemPrice[quantities[j]] = fileA[uniqueItems[i]][quantities[j]]
+          }
+        }
+        /* Item Exists in both */
+        else {
+          for (let j = 0; j < quantities.length; j++) {
+            let A = fileA[uniqueItems[i]]
+            let B = fileB[uniqueItems[i]]
+
+            if (A[quantities[j]] === undefined) {
+              individualItemPrice[quantities[j]] = B[quantities[j]]
+            } else if (B[quantities[j]] === undefined) {
+              individualItemPrice[quantities[j]] = A[quantities[j]]
+            } else {
+              individualItemPrice[quantities[j]] = A[quantities[j]] < B[quantities[j]] ? A[quantities[j]] : B[quantities[j]]
+            }
+          }
+        }
+
+        fileA[uniqueItems[i]] = individualItemPrice
       }
     }
+
 
     /* Write and Download File */
-    console.log(headers)
-
     let keys = Object.keys(fileA)
-
     let wbData = []
 
-    /* Create first row to format rest of datasheet */
-    let fr = {}
-    for (let i = 0; i < headers.length; i++) {
-      fr[headers[i]] = '-'
-    }
-    wbData.push(fr)
 
     for (let i = 0; i < keys.length; i++) {
       let rowData = {}
 
       /* Part Name */
-      rowData[headers[0]] = keys[i]
+      rowData[titleColumn] = keys[i]
 
       /* Quantity - Prices */
       let priceList = fileA[keys[i]]
-      for (let j = 0; j < priceList.length; j++) {
-        if (!isNaN(parseFloat(priceList[j]))) {
-          rowData[headers[j + 1]] = priceList[j] * prices[j]
+
+      let quantityArr = Object.keys(priceList)
+      let priceArr = Object.values(priceList)
+
+      for (let j = 0; j < quantityArr.length; j++) {
+        if (!isNaN(parseFloat(priceArr[j]))) {
+          rowData["[" + quantityArr[j].toString() + "]"] = priceArr[j] * q[quantityArr[j]]
         }
       }
-
       wbData.push(rowData)
     }
-
-    console.log(wbData)
 
     const ws = XLSX.utils.json_to_sheet(wbData)
     let wb = { Sheets: { 'data': ws }, SheetNames: ['data'] }
@@ -121,11 +131,17 @@ const App = () => {
     FileSaver.saveAs(dd, 'Data.xlsx');
   }
 
-  const updateMultiplier = (mult, i) => {
-    let newPriceList = prices.slice()
-    newPriceList[i] = parseFloat(mult)
-    console.log(newPriceList)
-    setPrices(newPriceList)
+  const updateMultiplier = (mult, value) => {
+    console.log(mult, value)
+    let tmp = JSON.parse(JSON.stringify(q))
+    if (mult.toString().length === 0) {
+      tmp[value] = 1
+      setQs(tmp)
+    } else {
+      tmp[value] = parseFloat(mult)
+      setQs(tmp)
+    }
+    console.log(tmp)
   }
 
 
@@ -137,96 +153,42 @@ const App = () => {
       <div style={{ paddingBottom: '20px', display: 'flex', flexDirection: 'row' }}>
         <div>
           <div style={{ fontWeight: 'bold', fontSize: '14px' }}>File 1</div>
-          <input type="file" onChange={(event) => onFileChange(event)} />
+          <input type="file" onChange={(event) => onFileChange(event, true)} />
         </div>
         <div>
           <div style={{ fontWeight: 'bold', fontSize: '14px' }}>File 2</div>
-          <input type="file" onChange={(event) => onFileChange2(event)} disabled={Object.keys(fileA).length === 0}/>
+          <input type="file" onChange={(event) => onFileChange(event, false)} disabled={Object.keys(fileA).length === 0} />
         </div>
       </div>
       <div style={{ paddingBottom: '30px' }}>
         <h2>
           Mark Up Value <span style={{ color: 'red', fontSize: '13px' }}>(numbers/ decimal only)</span>
         </h2>
-        <div>
-          <label style={{ paddingRight: '10px' }}>
-            Q10
-          </label>
-          <input type="number" onChange={(e) => updateMultiplier(e.target.value, 0)} value={prices[0]} />
-        </div>
-        <div>
-          <label style={{ paddingRight: '10px' }}>
-            Q25
-          </label>
-          <input type="number" onChange={(e) => updateMultiplier(e.target.value, 1)} value={prices[1]} />
-        </div>
-        <div>
-          <label style={{ paddingRight: '10px' }}>
-            Q50
-          </label>
-          <input type="number" onChange={(e) => updateMultiplier(e.target.value, 2)} value={prices[2]} />
-        </div>
-        <div>
-          <label style={{ paddingRight: '10px' }}>
-            Q100
-          </label>
-          <input type="number" onChange={(e) => updateMultiplier(e.target.value, 3)} value={prices[3]} />
-        </div>
-        <div>
-          <label style={{ paddingRight: '10px' }}>
-            Q250
-          </label>
-          <input type="number" onChange={(e) => updateMultiplier(e.target.value, 4)} value={prices[4]} />
-        </div>
-        <div>
-          <label style={{ paddingRight: '10px' }}>
-            Q500
-          </label>
-          <input type="number" onChange={(e) => updateMultiplier(e.target.value, 5)} value={prices[5]} />
-        </div>
-        <div>
-          <label style={{ paddingRight: '10px' }}>
-            Q1000
-          </label>
-          <input type="number" onChange={(e) => updateMultiplier(e.target.value, 6)} value={prices[6]} />
-        </div>
-        <div>
-          <label style={{ paddingRight: '10px' }}>
-            Q2500
-          </label>
-          <input type="number" onChange={(e) => updateMultiplier(e.target.value, 7)} value={prices[7]} />
-        </div>
-        <div>
-          <label style={{ paddingRight: '10px' }}>
-            Q3000
-          </label>
-          <input type="number" onChange={(e) => updateMultiplier(e.target.value, 8)} value={prices[8]} />
-        </div>
-        <div>
-          <label style={{ paddingRight: '10px' }}>
-            Q5000
-          </label>
-          <input type="number" onChange={(e) => updateMultiplier(e.target.value, 9)} value={prices[9]} />
-        </div>
-        <div>
-          <label style={{ paddingRight: '10px' }}>
-            Q10000
-          </label>
-          <input type="number" onChange={(e) => updateMultiplier(e.target.value, 10)} value={prices[10]} />
-        </div>
-        <div>
-          <label style={{ paddingRight: '10px' }}>
-            Q25000
-          </label>
-          <input type="number" onChange={(e) => updateMultiplier(e.target.value, 11)} value={prices[11]} />
-        </div>
+        {
+          Object.keys(q).map((value) => {
+            return (
+              <div key={value}>
+                <label style={{ paddingRight: '10px' }}>
+                  Q - {value}
+                </label>
+                <input type="number" onChange={(e) => updateMultiplier(e.target.value, value)} value={q[value]} />
+              </div>
+            )
+          })
+        }
+        {
+          Object.keys(q).length === 0 &&
+          <div>
+            <h4 style={{ color: "#333333", textTransform: "uppercase" }}>[Multiplier fields will display once file is uploaded]</h4>
+          </div>
+        }
         <button onClick={() => downloadFile()} style={{ marginTop: '20px' }} disabled={Object.keys(fileA).length === 0}>
           Download
         </button>
       </div>
-      <div>
+      <div style={{ justifyContent: 'center', alignItems: 'center', display: 'flex', flexDirection: 'column' }}>
         <h3>Your Uploaded File <span style={{ fontWeight: 'bold', color: 'red', fontSize: '20px' }}>MUST</span> follow the following format: </h3>
-        <img src={require('./assets/Format.png')} />
+        <img alt="Sample Format" src={require('./assets/Format.png')} width={"80%"} style={{ borderRadius: "10px" }} />
       </div>
     </div>
   );
