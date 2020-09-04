d48ee178ff0cc1f77ae92c6935e6d3f2b5149cb4
Darryl Leong <darryleong95@gmail.com>
2020-09-04 12:33:28 +0800

    Updates

--- precache-manifest.ddba30fe0ca0c7c9ed67706aad2e60c6.js
+++ precache-manifest.ddba30fe0ca0c7c9ed67706aad2e60c6.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "895054765e02fda7ad9c7fe9655265ef",
+    "revision": "e9efbc7e61d1db54fba81d4e2bd67729",
     "url": "/excel-automation/index.html"
   },
   {
-    "revision": "024a473a2e3bf314cd39",
+    "revision": "cd052aaa279f22630dbf",
     "url": "/excel-automation/static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/excel-automation/static/js/2.25d120de.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "024a473a2e3bf314cd39",
-    "url": "/excel-automation/static/js/main.40fed0c5.chunk.js"
+    "revision": "cd052aaa279f22630dbf",
+    "url": "/excel-automation/static/js/main.8f3e9199.chunk.js"
   },
   {
     "revision": "b7b839d9712b32b75946",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/excel-automation/precache-manifest.f94c352f57e1ca590ead79b623255b3b.js"
+  "/excel-automation/precache-manifest.ddba30fe0ca0c7c9ed67706aad2e60c6.js"
 );
 
 self.addEventListener('message', (event) => {
